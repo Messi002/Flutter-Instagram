dedeff22f2333d449107f3fdcb6312a83fbda31d
Messi002 <rostandmessi2@gmail.com>
2023-03-11 21:24:29 +0100

    adding http package

--- lib/resources/storage_method.dart
+++ lib/resources/storage_method.dart
@@ -32,3 +32,14 @@ class StorageMethods {
     return downloadUrl;
   }
 }
+
+
+
+
+// Future<bool> uploadImage({required File file, required String userId}) =>
+//     FirebaseStorage.instance
+//         .ref(userId)
+//         .child(const Uuid().v4())
+//         .putFile(file)
+//         .then((_) => true)
+//         .catchError((_) => false);
