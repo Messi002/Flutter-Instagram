e675bd01a3016abcd41fee57ccf55a46f1d1a6b2
Messi002 <rostandmessi2@gmail.com>
2023-03-15 23:01:36 +0100

    designing comment card

--- lib/screens/comment_screen.dart
+++ lib/screens/comment_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/model/user_model.dart';
+import 'package:instagram/providers/user_provider.dart';
 import 'package:instagram/utils/colors.dart';
-
+import 'package:instagram/widgets/comment_card.dart';
+import 'package:provider/provider.dart';
 
 class CommentScreen extends StatefulWidget {
   const CommentScreen({super.key});
@@ -12,17 +15,53 @@ class CommentScreen extends StatefulWidget {
 class _CommentScreenState extends State<CommentScreen> {
   @override
   Widget build(BuildContext context) {
+    final UserModel _user = Provider.of<UserProvider>(context).getUser;
+
     return Scaffold(
       backgroundColor: AppColors.mobileBackgroundColor,
-      appBar: AppBar(title: const Text('Comment Screen'),),
-      bottomNavigationBar: SafeArea(child: Container(
-        height: kToolbarHeight,
-        margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-        padding: const EdgeInsets.only(left: 16, right: 8),
-        child: Row(
-          children: [],
+      appBar: AppBar(
+        title: const Text('Comment Screen'),
+      ),
+      body: CommentCard(),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: kToolbarHeight,
+          margin:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          padding: const EdgeInsets.only(left: 16, right: 8),
+          child: Row(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(_user.photoUrl),
+                radius: 18,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 8),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Comment as ${_user.username}',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(color: Colors.blueAccent),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),),
+      ),
     );
   }
 }
---
+++ lib/widgets/comment_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class CommentCard extends StatefulWidget {
+  const CommentCard({super.key});
+
+  @override
+  State<CommentCard> createState() => _CommentCardState();
+}
+
+class _CommentCardState extends State<CommentCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1678811118543-14d19cb00508?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80'),
+            radius: 18,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'username',
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: 'some description to be added here',
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4),
+                    child: Text(
+                      '21/23/2002',
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Icon(
+              Icons.favorite,
+              size: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
