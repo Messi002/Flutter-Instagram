7094701aea9d9d8239e15ca83b60f94643813a85
Messi002 <rostandmessi2@gmail.com>
2023-03-24 04:22:07 +0100

    added and working on search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +14,7 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   late final TextEditingController _searchController;
+  bool isShowUsers = false;
 
   @override
   void initState() {
@@ -33,13 +36,44 @@ class _SearchScreenState extends State<SearchScreen> {
         title: TextFormField(
           controller: _searchController,
           decoration: const InputDecoration(labelText: 'Search for a user...'),
-          onFieldSubmitted: (String _){},
+          onFieldSubmitted: (String _) {
+            log(_);
+            setState(() {
+              isShowUsers = true;
+            });
+          },
         ),
       ),
-
       body: FutureBuilder(
-        future: FirebaseFirestore.instance.collection('users').where('username', isGreaterThanOrEqualTo: _searchController.text).get(),
-        builder:),
+        future: FirebaseFirestore.instance
+            .collection('users')
+            .where('username', isEqualTo: _searchController.text.trim())
+            .get(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              final item = snapshot.data!.docs[index];
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(item['photoUrl']),
+                ),
+                title: Text(item['username']),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
+
+
+//5 : 39 : 01
